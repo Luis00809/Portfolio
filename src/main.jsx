6bcb24abf239f89d05e7fe9dbbeb6083dffdfb6c
Luis00809 <davidcarvajal008@gmail.com>
2023-11-04 08:35:21 -0700

    adds a footer

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDom from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import './index.css';
+import './index.css';
 
 import App from './App';
 import About from './pages/About';
