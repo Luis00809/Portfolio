746bb47482613ecb86fa42219b333cd1d8a9efee
Luis00809 <davidcarvajal008@gmail.com>
2023-11-05 00:52:08 -0700

    sets up image flex

--- src/components/UI/CardList.jsx
+++ src/components/UI/CardList.jsx
@@ -1,10 +1,18 @@
+
+const styles = {
+    size: {
+        width: '250px',
+        height: '250px'
+    }
+}
+
 function Cardlist(props) {
     return (
         <div className="container">
-            <div className="row">
+            <div className="row d-flex flex-wrap gx-2 ">
                 {props.project.map(item => (
-                    <div key={item.id}>
-                        <img src={item.src} alt={item.name} />
+                    <div key={item.id} className="col-lg-4 col-md-5 col-sm-6">
+                        <img style={styles.size} src={item.src} alt={item.name} />
                         <h2>{item.name}</h2>
                         <p>{item.description}</p>
                     </div>
