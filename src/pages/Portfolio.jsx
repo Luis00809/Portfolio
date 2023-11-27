febc16edb093e5d6c6b79ad789b05bc03ad8792a
Luis00809 <davidcarvajal008@gmail.com>
2023-11-26 22:03:37 -0800

    moves public folder

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,21 +5,21 @@ const project = [
         id: 1,
         name: "Vidya",
         description: "Video Game Reviewer",
-        src: '../src/public/vidya2.png',
+        src: '../public/vidya2.png',
         link: "https://luis00809.github.io/Group-3-project/"
     },
     {
         id: 2,
         name: "codeMantic",
         description: "Programmer Dating App",
-        src: '../src/public/codeMantic2.png',
+        src: '../public/codeMantic2.png',
         link: "https://codemantic-e7e316dea174.herokuapp.com/"
     },
     {
         id: 3,
         name: "Day Scheduler",
         description: "Task tracking",
-        src: '../src/public/scheduler2.png',
+        src: '../public/scheduler2.png',
         link: "https://luis00809.github.io/Day-scheduler/"
 
     },
@@ -27,7 +27,7 @@ const project = [
         id: 4,
         name: "Note Taker",
         description: "Note Taker",
-        src: '../src/public/noteTaker2.png',
+        src: '../public/noteTaker2.png',
         link: "https://note-taker-system-05e719970f34.herokuapp.com/notes"
 
     },
@@ -35,14 +35,14 @@ const project = [
         id: 5,
         name: "Weather Tracker",
         description: "Global Weather Report",
-        src: '../src/public/weatherTracker2.png',
+        src: '../public/weatherTracker2.png',
         link: "https://luis00809.github.io/Weather-Application/"
     },
     {
         id: 6,
         name: "Password Generator",
         description: "Create a random password",
-        src: '../src/public/PasswordGenerator.png',
+        src: '../public/PasswordGenerator.png',
         link: "https://luis00809.github.io/password-generator/"
 
     }
